65a2c6c9845766a3e648ef62cff075505688dbe5
Naren Thiagarajan <narenst@gmail.com>
2019-08-27 11:27:12 -0700

    Ensure instance is ready before attaching volume

--- infinity/command/volume/attach.py
+++ infinity/command/volume/attach.py
@@ -34,10 +34,20 @@ def attach(volume_id, instance_id):
         if attachment['DeviceName'] == SECONDARY_EBS_DEVICE_NAME:
             raise Exception(f"Instance {instance_id} already has a secondary volume attached to it")
 
-    # Check if instance is stopped
+    # Check if instance is stopped or running
     if not instance.state['Name'] in ['stopped', 'running']:
         raise Exception(f"Volume can be mounted only to a stopped or running instance. "
                         f"Current state: {instance.state['Name']}")
+
+    # Check if the user data script is finished running
+    ssh_private_key_path = get_infinity_settings().get('ssh_private_key_path')
+    if instance.state['Name'] == 'running':
+        # Ignore the KeyChecking since the hosts are trusted
+        ssh_command = f"ssh -oStrictHostKeyChecking=no -i {ssh_private_key_path} " \
+                      f"ubuntu@{instance.public_ip_address} ls /etc/load_volume.sh"
+        code = os.system(ssh_command)
+        if code != 0:
+            raise Exception(f"Instance does not have the data mount script. Try again after a few seconds")
 
     print("Attaching volume to the instance...")
 
@@ -55,9 +65,6 @@ def attach(volume_id, instance_id):
     if instance.state['Name'] == 'running':
         print("Mounting the disk to the running machine at: /data")
         # Connect to the machine and run /etc/load_volume.sh script
-        ssh_private_key_path = get_infinity_settings().get('ssh_private_key_path')
-
-        # Ignore the KeyChecking since the hosts are trusted
         ssh_command = f"ssh -oStrictHostKeyChecking=no -i {ssh_private_key_path} " \
                       f"ubuntu@{instance.public_ip_address} sudo /etc/load_volume.sh"
 
