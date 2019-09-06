a2290a995803f35e4c824c450ddb80b8a3080a26
Naren Thiagarajan <narenst@gmail.com>
2019-09-06 11:12:36 -0700

    Local analytics will not print anything

--- infinity/analytics/local.py
+++ infinity/analytics/local.py
@@ -6,7 +6,9 @@ class LocalAnalytics(Analytics):
     Implementation of Analytics for Development.
     """
     def invoke_register_user(self, user_id, user_properties):
-        print(f"Registering user: {user_id} with properties: {user_properties}")
+        # print(f"Registering user: {user_id} with properties: {user_properties}")
+        pass
 
     def invoke_track_event(self, user_id, event_name, event_properties):
-        print(f"Sending event: {event_name} with properties: {event_properties}")
+        # print(f"Sending event: {event_name} with properties: {event_properties}")
+        pass
